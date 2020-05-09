15ceeb2e608934d2262721d21aeb998d36e50a1a
ldd-bit <2775705431@qq.com>
2020-05-09 20:34:50 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 加载全局样式
+import './style/index.less'
+// 加载Vant组件库
+import Vant from 'vant'
+// 加载Vant组件库样式
+import 'vant/lib/index.css'
+import 'amfe-flexible'
+// 全局注册Vant中的组件
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
